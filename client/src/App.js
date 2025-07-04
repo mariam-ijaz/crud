d891ab5a9421aa96cb7220c5d40c11c897c6b895
mariam-ijaz <mariamijaz34@gmail.com>
2025-07-03 22:16:32 -0700

    Add working code

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import AddUser from "./adduser/AddUser";
+import User from "./getuser/User";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Update from "./updateuser/Update";
 
 function App() {
+  const route = createBrowserRouter([
+    {
+      path: "/",
+      element: <User />,
+    },
+    {
+      path: "/add",
+      element: <AddUser />,
+    },
+    {
+      path: "/update/:id",
+      element: <Update />,
+    },
+  ]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={route}></RouterProvider>
     </div>
   );
 }
